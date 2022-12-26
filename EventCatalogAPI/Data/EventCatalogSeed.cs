7dd3ab1dd1ceddbce18ada4ce78b95baf3240b5e
Heather Yi <heather.yi.work@gmail.com>
2022-12-25 21:28:41 -0500

    Catelog seed data and pics

--- EventCatalogAPI/Data/EventCatalogSeed.cs
+++ EventCatalogAPI/Data/EventCatalogSeed.cs
@@ -32,21 +32,21 @@ namespace EventCatalogAPI.Data
         {
             return new List<EventItem>()
             {
-                new EventItem { EventLocationId = 2, EventCategoryId = 3, Description = "A ring that has been around for over 100 years", Name = "World Star", Price = 199.5M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/1" },
-                new EventItem { EventLocationId = 1, EventCategoryId = 2, Description = "will make you world champions", Name = "White Line", Price = 88.50M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/2" },
-                new EventItem { EventLocationId = 2, EventCategoryId = 3, Description = "You have already won gold medal", Name = "Prism White", Price = 129, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/3" },
-                new EventItem { EventLocationId = 2, EventCategoryId = 2, Description = "Olympic runner", Name = "Foundation Hitech", Price = 12, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/4" },
-                new EventItem { EventLocationId = 2, EventCategoryId = 1, Description = "Roslyn Red Sheet", Name = "Roslyn White", Price = 188.5M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/5" },
-                new EventItem { EventLocationId = 2, EventCategoryId = 2, Description = "Lala Land", Name = "Blue Star", Price = 112, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/6" },
-                new EventItem { EventLocationId = 2, EventCategoryId = 1, Description = "High in the sky", Name = "Roslyn Green", Price = 212, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/7" },
-                new EventItem { EventLocationId = 1, EventCategoryId = 1, Description = "Light as carbon", Name = "Deep Purple", Price = 238.5M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/8" },
-                new EventItem { EventLocationId = 1, EventCategoryId = 2, Description = "High Jumper", Name = "Antique Ring", Price = 129, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/9" },
-                new EventItem { EventLocationId = 2, EventCategoryId = 3, Description = "Dunker", Name = "Elequent", Price = 12, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/10" },
-                new EventItem { EventLocationId = 1, EventCategoryId = 2, Description = "All round", Name = "Inredeible", Price = 248.5M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/11" },
-                new EventItem { EventLocationId = 2, EventCategoryId = 1, Description = "Pricesless", Name = "London Sky", Price = 412, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/12" },
-                new EventItem { EventLocationId = 3, EventCategoryId = 3, Description = "You ar ethe star", Name = "Elequent", Price = 123, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/13" },
-                new EventItem { EventLocationId = 3, EventCategoryId = 2, Description = "A ring popular in the 16th and 17th century in Western Europe that was used as an engagement wedding ring", Name = "London Star", Price = 218.5M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/14" },
-                new EventItem { EventLocationId = 3, EventCategoryId = 1, Description = "A floppy, bendable ring made out of links of metal", Name = "Paris Blues", Price = 312, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/15" }
+                new EventItem { EventLocationId = 2, EventCategoryId = 3, Description = "Our company is saying farewell to our beloved coworker!", Name = "Happy Retirement", Date =Convert.ToDateTime("04/21/2023"), StartTime=Convert.ToDateTime("6:30PM"), EndTime=Convert.ToDateTime("8:30PM"), Price = 20.5M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/1" },
+                new EventItem { EventLocationId = 1, EventCategoryId = 2, Description = "Welcoming the newest addition to the workplace!", Name = "Coworker's Baby Shower",Date =Convert.ToDateTime("05/01/2023"), StartTime=Convert.ToDateTime("12:30PM"), EndTime=Convert.ToDateTime("2:30PM"), Price = 15.5M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/2" },
+                new EventItem { EventLocationId = 2, EventCategoryId = 3, Description = "Making memories with the company", Name = "Company Team Building",Date =Convert.ToDateTime("07/22/2023"), StartTime=Convert.ToDateTime("7:30AM"), EndTime=Convert.ToDateTime("10:30AM"), Price = 30M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/3" },
+                new EventItem { EventLocationId = 2, EventCategoryId = 2, Description = "Wind down from a long work week", Name = "Workplace Retreat",Date =Convert.ToDateTime("02/05/2023"), StartTime=Convert.ToDateTime("10:00AM"), EndTime=Convert.ToDateTime("9:00PM"), Price = 25M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/4" },
+                new EventItem { EventLocationId = 2, EventCategoryId = 1, Description = "Join us for our annual convention", Name = "Keyboard Convention",Date =Convert.ToDateTime("11/14/2023"), StartTime=Convert.ToDateTime("12:30PM"), EndTime=Convert.ToDateTime("5:30PM"), Price = 20.5M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/5" },
+                new EventItem { EventLocationId = 2, EventCategoryId = 2, Description = "Sleep under the stars", Name = "Community Camping Trip", Date =Convert.ToDateTime("08/30/2023"), StartTime=Convert.ToDateTime("07:00AM"), EndTime=Convert.ToDateTime("11:30PM"),Price = 15M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/6" },
+                new EventItem { EventLocationId = 2, EventCategoryId = 1, Description = "Fun for the whole family!", Name = "Cookie Decorating Party",Date =Convert.ToDateTime("12/01/2023"), StartTime=Convert.ToDateTime("10:00AM"), EndTime=Convert.ToDateTime("3:00PM"), Price = 30M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/7" },
+                new EventItem { EventLocationId = 1, EventCategoryId = 1, Description = "Be here for our grand opening", Name = "Amusement Park Opening",Date =Convert.ToDateTime("07/16/2023"), StartTime=Convert.ToDateTime("11:30AM"), EndTime=Convert.ToDateTime("9:30PM"), Price = 55M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/8" },
+                new EventItem { EventLocationId = 1, EventCategoryId = 2, Description = "Welcoming all members of the family", Name = "Family Reunion",Date =Convert.ToDateTime("03/01/2023"), StartTime=Convert.ToDateTime("1:30PM"), EndTime=Convert.ToDateTime("7:30PM"), Price = 20M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/9" },
+                new EventItem { EventLocationId = 2, EventCategoryId = 3, Description = "The IceHouse is open for season!", Name = "Ice Skating Event",Date =Convert.ToDateTime("12/11/2023"), StartTime=Convert.ToDateTime("10:45AM"), EndTime=Convert.ToDateTime("5:45PM"), Price = 15M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/10" },
+                new EventItem { EventLocationId = 1, EventCategoryId = 2, Description = "Celebrating the first day of summer", Name = "Summer Pool Party",Date =Convert.ToDateTime("06/08/2023"), StartTime=Convert.ToDateTime("10:00PM"), EndTime=Convert.ToDateTime("6:30PM"), Price = 20.5M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/11" },
+                new EventItem { EventLocationId = 2, EventCategoryId = 1, Description = "Bring blankets and popcorn", Name = "Outdoor Movie Night",Date =Convert.ToDateTime("07/16/2023"), StartTime=Convert.ToDateTime("7:30PM"), EndTime=Convert.ToDateTime("9:30PM"), Price = 15.5M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/12" },
+                new EventItem { EventLocationId = 3, EventCategoryId = 3, Description = "Enjoy a relaxing night", Name = "Paint and Sip",Date =Convert.ToDateTime("01/16/2023"), StartTime=Convert.ToDateTime("8:00PM"), EndTime=Convert.ToDateTime("10:00PM"), Price = 35M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/13" },
+                new EventItem { EventLocationId = 3, EventCategoryId = 2, Description = "Show off your knowledge skills", Name = "Trivia Night",Date =Convert.ToDateTime("09/02/2023"), StartTime=Convert.ToDateTime("8:30PM"), EndTime=Convert.ToDateTime("10:30PM"), Price = 10.5M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/14" },
+                new EventItem { EventLocationId = 3, EventCategoryId = 1, Description = "Ring in the new year!", Name = "New Years Eve Countdown", Date =Convert.ToDateTime("12/31/2023"), StartTime=Convert.ToDateTime("9:00PM"), EndTime=Convert.ToDateTime("12:00AM"),Price = 30M, PictureUrl = "http://externalcatalogbaseurltobereplaced/api/pic/15" }
             };
         }
 
@@ -54,9 +54,9 @@ namespace EventCatalogAPI.Data
         {
             return new List<EventCategory>
             {
+                new EventCategory {Category = "Work"},
                 new EventCategory {Category = "Kids"},
-                new EventCategory {Category = "Spiritual"},
-                new EventCategory {Category = "Educational"}
+                new EventCategory {Category = "Social"}
             };
         }
 
