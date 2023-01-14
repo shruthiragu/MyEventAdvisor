098002055804538fb58e30858ca39effd340ad1d
kanaanayt <kanaanabukhadra@gmail.com>
2023-01-14 16:07:22 +0300

    done with token integration

---
+++ WebMvc/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace WebMvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        public async Task<IActionResult> SignIn(string returnUrl)
+        {
+            var idToken = await HttpContext.GetTokenAsync("id_token");
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            return RedirectToAction(nameof(EventCatalogController.About), "EventCatalog");
+        }
+        public async Task<IActionResult> Signout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+            return new SignOutResult(OpenIdConnectDefaults.AuthenticationScheme, new AuthenticationProperties
+            {
+                RedirectUri = Url.Action(nameof(EventCatalogController.Index), "EventCatalog")
+            });
+        }
+    }
+}
