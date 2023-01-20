5fee2f9a95dcc8cf8705147605783647e6defa54
shruthiragu <115894437+shruthiragu@users.noreply.github.com>
2023-01-19 16:52:27 -0800

    Cart microservice

---
+++ CartApi/Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿namespace CartApi.Models
+{
+    public class Cart
+    {
+        public string BuyerId { get; set; }
+        public List<CartItem> Items { get; set; }
+        public Cart() { }
+        public Cart (string cartId)
+        {
+            BuyerId = cartId;
+            Items = new List<CartItem>();
+        }
+    }
+}
