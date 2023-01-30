889cf246fde78184ba6795acef03d58ec7e53404
shruthiragu <115894437+shruthiragu@users.noreply.github.com>
2023-01-29 16:01:41 -0800

    Order Microservice Backend Code

---
+++ OrderApi/Controllers/OrdersController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OrderApi.Data;
+using OrderApi.Models;
+using System.Net;
+
+namespace OrderApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class OrdersController : ControllerBase
+    {
+        private readonly OrdersContext _ordersContext;
+        private readonly ILogger<OrdersController> _logger;
+        private readonly IConfiguration _config;
+        public OrdersController(OrdersContext ordersContext, ILogger<OrdersController> logger, IConfiguration config)
+        {
+            _ordersContext = ordersContext;
+            _logger = logger;
+            _config = config;
+        }
+
+        //api/order/getorder/{id}
+        [HttpGet("{id}", Name = "[action]")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> GetOrder(int id)
+        {
+            var item = await _ordersContext.Orders
+                                        .Include(x => x.OrderItems)
+                                        .SingleOrDefaultAsync(ci => ci.OrderId == id);
+            if (item != null)
+            {
+                return Ok(item);
+            }
+            return NotFound();
+
+        }
+
+        //api/order/new
+        [Route("new")]
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        public async Task<IActionResult> CreateOrder([FromBody] Order order)
+        {
+            order.OrderStatus = OrderStatus.Preparing;
+            order.OrderDate = DateTime.UtcNow;
+
+            _logger.LogInformation("In create order");
+            _logger.LogInformation("Order: " + order.UserName);
+
+            _ordersContext.Orders.Add(order);
+            _ordersContext.OrderItems.AddRange(order.OrderItems);
+
+            _logger.LogInformation("Orders adding to context");
+            _logger.LogInformation("Saving...");
+
+            try
+            {
+                await _ordersContext.SaveChangesAsync();
+                return Ok(new { order.OrderId });
+            } catch (DbUpdateException ex)
+            {
+                _logger.LogError("Order saving failed..." + ex.Message);
+                return BadRequest();
+            }
+
+        }
+    }
+}
---
+++ OrderApi/Models/OrderItem.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OrderApi.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string PictureUrl { get; set; }
+        public int ProductId { get; private set; }
+        public decimal UnitPrice { get; set; }
+        public int Units { get; set; }
+        //public virtual Order order { get; set; }
+        public int OrderId { get; set; } //Foreign key associated with the order
+
+        public OrderItem (string productName, string pictureUrl, int productId, decimal unitPrice, int units)
+        {           
+            ProductName = productName;
+            PictureUrl = pictureUrl;
+            ProductId = productId;
+            UnitPrice = unitPrice;
+            Units = units;            
+        }
+
+        public void SetPictureUrl(string pictureUrl)
+        {
+            if (!String.IsNullOrEmpty(pictureUrl))
+            {
+                PictureUrl = pictureUrl;
+            }
+        }
+
+        public void AddUnits(int units)
+        {
+            Units += units;
+        }
+    }
+}
