4ca8456aa4ef1d5d5cb2275c0405ffb4c1bcf953
Heather Yi <heather.yi.work@gmail.com>
2023-01-30 14:33:19 -0500

    adding OrderAPI

---
+++ OrderAPI/Data/OrdersContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderAPI.Models;
+
+namespace OrderAPI.Data
+{
+
+    public class OrdersContext : DbContext
+
+    {
+        public OrdersContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+    }
+}
